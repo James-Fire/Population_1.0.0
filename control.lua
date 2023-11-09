78a8d7bab851d3da889e5c6d0c8a118c213c1895
James-Fire <41241412+James-Fire@users.noreply.github.com>
2023-11-08 21:22:10 -0700

    Control Fixes
    Housing now removes it's beacon all the time when removed
    Housing now counts water separately from other tiles
    Housing now has a much higher limit for being affected by tiles
    All printing commented out

--- control.lua
+++ control.lua
@@ -37,58 +37,9 @@ end
 --iscrafting() Table entry on an entity that tracks whether it is crafting
 
 
-
-
-script.on_nth_tick(PeriodicUpdateInterval, function(event)
-	--game.print("Update global update list and interval")
-	global.counter = 0
-	global.UpdateList = { }
-	global.UpdateInterval = PeriodicUpdateInterval/((table_size(global.HousingList)+table_size(global.FarmList))+1)
-	--game.print("Update Interval: "..tostring(global.UpdateInterval))
-	for i, House in pairs(global.HousingList) do
-		if House.valid then
-			table.insert(global.UpdateList, House)
-		else
-			global.HousingList[i] = nil
-		end
-	end
-	for i, Farm in pairs(global.FarmList) do
-		if Farm.valid then
-			table.insert(global.UpdateList, Farm)
-		else
-			global.FarmList[i] = nil
-		end
-	end
-	--game.print(serpent.block(global.UpdateList))
-end)
-
-local function doSomething()
-	global.counter = global.counter + 1
-	--game.print("Global Counter: "..tostring(global.counter))
-	if global.UpdateList[global.counter] == nil then
-	else
-		--game.print(serpent.block(global.UpdateList[global.counter]))
-		if global.UpdateList[global.counter].name:find("-farm", 1, true) or global.UpdateList[global.counter].name == "fishery" then
-			--game.print("Updating a "..tostring(global.UpdateList[global.counter].name))
-			ManageFarmingBeacon(global.UpdateList[global.counter], global.UpdateList[global.counter].surface, global.UpdateList[global.counter].force, 3)
-		elseif global.UpdateList[global.counter].name:find("-house", 1, true) or global.UpdateList[global.counter].name:find("-tower", 1, true) or global.UpdateList[global.counter].name:find("-rise", 1, true) then
-			--game.print("Updating a "..tostring(global.UpdateList[global.counter].name))
-			ManageHousingBeacon(global.UpdateList[global.counter], global.UpdateList[global.counter].surface, global.UpdateList[global.counter].force, MathData.HousingSize[3])
-		end
-	end
-end
-
-script.on_event(defines.events.on_tick, function(event)
-	action = global.queue[event.tick]
-	if not action then return end
-	doSomething()
-	global.queue[event.tick] = nil
-	global.queue[event.tick + global.UpdateInterval] = action --Do the action every 60 ticks, (1s)
-	--game.print("tick")
-end)
-
 local function OnInit()
 	global.Good_Water_Tiles_List = { }
+	global.Bad_Water_Tiles_List = { }
 	global.Good_Tiles_List = { }
 	global.Bad_Tiles_List = { }
 	global.Meh_Tiles_List = { }
@@ -97,7 +48,7 @@ local function OnInit()
 	
 	for i, tile in pairs(game.tile_prototypes) do
 		if tile.name:find("water", 1, true) and tile.name:find("green", 1, true) and not CheckTableValue(tile.name, Processed_Tiles_List) and not CheckTableValue(tile.name, global.Good_Tiles_List) and not CheckTableValue(tile.name, global.Bad_Tiles_List) and not CheckTableValue(tile.name, global.Meh_Tiles_List) then
-			table.insert(global.Bad_Tiles_List, tile.name)
+			table.insert(global.Bad_Water_Tiles_List, tile.name)
 			table.insert(Processed_Tiles_List, tile.name)
 		elseif tile.name:find("nuclear", 1, true) and not CheckTableValue(tile.name, Processed_Tiles_List) and not CheckTableValue(tile.name, global.Good_Tiles_List) and not CheckTableValue(tile.name, global.Bad_Tiles_List) and not CheckTableValue(tile.name, global.Meh_Tiles_List) then
 			table.insert(global.Bad_Tiles_List, tile.name)
@@ -137,77 +88,9 @@ local function OnInit()
 	global.UpdateList = { }
 	
 	global.counter = 0
-	global.queue = {}
-	global.queue[60] = {toDo = doSomething}
+	global.queue = 0
 end
 
-local function on_new_entity(event)
-	local new_entity = event.created_entity or event.entity --Handle multiple event types
-	local surface = new_entity.surface
-	local position = new_entity.position
-	local force = new_entity.force
-	if new_entity.name:find("-farm", 1, true) then	
-		game.print("Found Farm")
-		ManageFarmingBeacon(new_entity, surface, force, 3)
-		if not CheckTableValue(new_entity,global.FarmList) then
-			game.print("Farm inserted into Global Table")
-			table.insert(global.FarmList, new_entity)
-		end
-	elseif new_entity.name == "fishery" then
-		ManageFarmingBeacon(new_entity, surface, force, 3)
-		if not CheckTableValue(new_entity,global.FarmList) then
-			table.insert(global.FarmList, new_entity)
-		end
-	elseif new_entity.name == "sf-house" then
-		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
-		if not CheckTableValue(new_entity,global.HousingList) then
-			table.insert(global.HousingList, new_entity)
-		end
-	elseif new_entity.name == "mf-house" then
-		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
-		if not CheckTableValue(new_entity,global.HousingList) then
-			table.insert(global.HousingList, new_entity)
-		end
-	elseif new_entity.name == "low-rise" then
-		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
-		if not CheckTableValue(new_entity,global.HousingList) then
-			table.insert(global.HousingList, new_entity)
-		end
-	elseif new_entity.name == "ap-tower" then
-		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
-		if not CheckTableValue(new_entity,global.HousingList) then
-			table.insert(global.HousingList, new_entity)
-		end
-	elseif new_entity.name == "arcology-tower" then
-		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
-		if not CheckTableValue(new_entity,global.HousingList) then
-			table.insert(global.HousingList, new_entity)
-		end
-	end
-end
-
-local function on_remove_entity(event)
-	local entity = event.created_entity or event.entity --Handle multiple event types
-	if not entity then return end
-	local surface = entity.surface
-	local position = entity.position
-	local force = entity.force
-	if entity.name == "sf-house" then
-		removeHiddenBeacon(surface, position, Housing_Beacon)
-	end
-	if entity.name == "mf-house" then
-		removeHiddenBeacon(surface, position, Housing_Beacon)
-	end
-	if entity.name == "low-rise" then
-		removeHiddenBeacon(surface, position, Housing_Beacon)
-	end
-	if entity.name == "ap-tower" then
-		removeHiddenBeacon(surface, position, Housing_Beacon)
-	end
-	if entity.name == "arcology-tower" then
-		removeHiddenBeacon(surface, position, Housing_Beacon)
-	end
-end
 
 local function removeHiddenBeacon(surface, position, name)
 	-- Remove Beacon for Housing
@@ -248,8 +131,8 @@ local function ManageFarmingBeaconModules(module_inventory, Score)
 			module_inventory.remove{name = Farming_Neg_Module, count = module_inventory.get_item_count(Farming_Neg_Module)}
 		end
 		local added_modules = round(math.abs(Score))
-		game.print("Rounded Score:"..tostring(Score))
-		game.print("Module Count:"..tostring(added_modules))
+		--game.print("Rounded Score:"..tostring(Score))
+		--game.print("Module Count:"..tostring(added_modules))
 		if added_modules < 0 then
 			added_modules = 0
 		end
@@ -322,7 +205,7 @@ end
 
 --Calculate terrain impact
 local function CalculateHousingBeacon(surface, position, force, radius)
-	local Tiles_Max = radius*radius/5 --You need this many Meh tiles to start having penalties, then you can have this many Meh tiles give penalties, and this is the max number of a single type of good or bad tile that have an effect
+	local Tiles_Max = radius*radius/2 --You need this many Meh tiles to start having penalties, then you can have this many Meh tiles give penalties, and this is the max number of a single type of good or bad tile that have an effect
 	local Score = 0
 	local TempScore = 0
 	local Terrain_Radius = 2*radius
@@ -378,10 +261,13 @@ local function CalculateHousingBeacon(surface, position, force, radius)
 			BadTiles = BadTiles + 1
 		elseif CheckTableValue(tile.name,global.Good_Tiles_List) then
 			GoodTiles = GoodTiles + 1
+		elseif CheckTableValue(tile.name,global.Bad_Water_Tiles_List) then
+			WaterTiles = WaterTiles - 1
 		elseif CheckTableValue(tile.name,global.Good_Water_Tiles_List) then
-			GoodTiles = GoodTiles + 1
+			WaterTiles = WaterTiles + 1
 		end
 	end
+	--game.print("Water Tile Count: "..tostring(WaterTiles))
 	--game.print("Good Tile Count: "..tostring(GoodTiles))
 	--game.print("Bad Tile Count: "..tostring(BadTiles))
 	--game.print("Meh Tile Count: "..tostring(MehTiles))
@@ -404,18 +290,19 @@ local function CalculateHousingBeacon(surface, position, force, radius)
 			Score = Score - TempScore*Per_Tile_Impact
 		end
 	end
+	Score = Score + WaterTiles
 	--game.print("After Tiles Score: "..tostring(Score))
 	--Account for pollution
 	local pollution = surface.get_pollution(position)
 	--game.print("Pollution: "..tostring(pollution))
 	Score = Score - pollution
 	if Score > Tiles_Max then
-		return Tiles_Max
+		Score = Score + Tiles_Max
 	elseif	Score < (Tiles_Max*-1) then
-		return (Tiles_Max*-1)
-	else
-		return Score
+		Score = Score - Tiles_Max
 	end
+	--game.print("Final Score: "..tostring(Score))
+	return Score
 end
 
 -- Set modules in hidden beacons for Terrain speed bonus
@@ -437,6 +324,7 @@ local function ManageHousingBeacon(entity, surface, force, radius)
 		local module_inventory = hiddenBeacon.get_module_inventory()
 		if module_inventory then
 			--game.print("Module Inventory is valid")
+			--game.print("Calc score for "..entity.name)
 			local Score = CalculateHousingBeacon(surface, hiddenBeacon.position, force, radius)
 			--game.print("Final House Score: "..tostring(Score))
 			ManageHousingBeaconModules(module_inventory, Score)
@@ -445,9 +333,9 @@ local function ManageHousingBeacon(entity, surface, force, radius)
 end
 -- Set modules in hidden beacons for Terrain farming speed bonus
 local function ManageFarmingBeacon(entity, surface, force, radius)
-	game.print("Updating farm at"..serpent.line(entity.position))
+	--game.print("Updating farm at"..serpent.line(entity.position))
 	if entity.valid then
-		log(entity.name.." is valid")
+		--log(entity.name.." is valid")
 		local hiddenBeacon = surface.find_entity(Housing_Beacon, entity.position)
 		--game.print("Beacon "..serpent.block(hiddenBeacon))
 		if hiddenBeacon == nil then
@@ -495,10 +383,127 @@ local function OnLoadSetup() --Unused, shouldn't be needed
 	end
 end
 
+local function on_new_entity(event)
+	local new_entity = event.created_entity or event.entity --Handle multiple event types
+	local surface = new_entity.surface
+	local position = new_entity.position
+	local force = new_entity.force
+	if new_entity.name:find("-farm", 1, true) then	
+		--game.print("Found Farm")
+		ManageFarmingBeacon(new_entity, surface, force, 3)
+		if not CheckTableValue(new_entity,global.FarmList) then
+			game.print("Farm inserted into Global Table")
+			table.insert(global.FarmList, new_entity)
+		end
+	elseif new_entity.name == "fishery" then
+		ManageFarmingBeacon(new_entity, surface, force, 3)
+		if not CheckTableValue(new_entity,global.FarmList) then
+			table.insert(global.FarmList, new_entity)
+		end
+	elseif new_entity.name == "sf-house" then
+		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
+		if not CheckTableValue(new_entity,global.HousingList) then
+			table.insert(global.HousingList, new_entity)
+		end
+	elseif new_entity.name == "mf-house" then
+		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
+		if not CheckTableValue(new_entity,global.HousingList) then
+			table.insert(global.HousingList, new_entity)
+		end
+	elseif new_entity.name == "low-rise" then
+		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
+		if not CheckTableValue(new_entity,global.HousingList) then
+			table.insert(global.HousingList, new_entity)
+		end
+	elseif new_entity.name == "ap-tower" then
+		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
+		if not CheckTableValue(new_entity,global.HousingList) then
+			table.insert(global.HousingList, new_entity)
+		end
+	elseif new_entity.name == "arcology-tower" then
+		ManageHousingBeacon(new_entity, surface, force, MathData.HousingSize[3])
+		if not CheckTableValue(new_entity,global.HousingList) then
+			table.insert(global.HousingList, new_entity)
+		end
+	end
+end
+
+local function on_remove_entity(event)
+	local entity = event.created_entity or event.entity --Handle multiple event types
+	if not entity then return end
+	local surface = entity.surface
+	local position = entity.position
+	local force = entity.force
+	if entity.name == "sf-house" or entity.name == "mf-house" or entity.name == "low-rise" or entity.name == "ap-tower" or entity.name == "arcology-tower" then
+		removeHiddenBeacon(surface, position, Housing_Beacon)
+	elseif entity.name:find("-farm", 1, true) or entity.name == "fishery" then
+		removeHiddenBeacon(surface, position, Housing_Beacon)
+	end
+end
+
+local function RemakeUpdateList()
+	--game.print("Update global update list and interval")
+	global.counter = 0
+	global.UpdateList = { }
+	global.UpdateInterval = PeriodicUpdateInterval/((table_size(global.HousingList)+table_size(global.FarmList))+1)
+	--game.print("Update Interval: "..tostring(global.UpdateInterval))
+	for i, House in pairs(global.HousingList) do
+		if House.valid then
+			table.insert(global.UpdateList, House)
+		else
+			global.HousingList[i] = nil
+		end
+	end
+	for i, Farm in pairs(global.FarmList) do
+		if Farm.valid then
+			table.insert(global.UpdateList, Farm)
+		else
+			global.FarmList[i] = nil
+		end
+	end
+	--game.print("New Update List:"..serpent.block(global.UpdateList))
+end
+
+local function OnNthTick()
+	--RemakeUpdateList()
+end
+
+local function HandleUpdate()
+	global.counter = global.counter + 1
+	--game.print("Global Counter: "..tostring(global.counter))
+	--game.print("Global Counter Entry: "..serpent.block(global.UpdateList[global.counter]))
+	if global.UpdateList[global.counter] ~= nil and global.UpdateList[global.counter].valid then
+		--game.print(serpent.block(global.UpdateList[global.counter]))
+		if global.UpdateList[global.counter].name:find("-farm", 1, true) or global.UpdateList[global.counter].name == "fishery" then
+			--game.print("Updating a "..tostring(global.UpdateList[global.counter].name))
+			ManageFarmingBeacon(global.UpdateList[global.counter], global.UpdateList[global.counter].surface, global.UpdateList[global.counter].force, 3)
+		elseif global.UpdateList[global.counter].name:find("-house", 1, true) or global.UpdateList[global.counter].name:find("-tower", 1, true) or global.UpdateList[global.counter].name:find("-rise", 1, true) then
+			--game.print("Updating a "..tostring(global.UpdateList[global.counter].name))
+			ManageHousingBeacon(global.UpdateList[global.counter], global.UpdateList[global.counter].surface, global.UpdateList[global.counter].force, MathData.HousingSize[3])
+		end
+	else
+		global.counter = 0
+		RemakeUpdateList()
+	end
+end
+
+script.on_event(defines.events.on_tick, function(event)
+	--game.print("pre tick")
+	global.queue = global.queue + 1
+	if (global.queue == 60) then
+		HandleUpdate()
+		global.queue = 0
+		--game.print("tick")
+	end
+end)
+
 script.on_event(defines.events.on_entity_destroyed, on_remove_entity)
 script.on_event(defines.events.on_entity_died, on_remove_entity)
+script.on_event(defines.events.on_robot_mined_entity, on_remove_entity)
+script.on_event(defines.events.on_player_mined_entity, on_remove_entity)
 script.on_event(defines.events.on_built_entity, on_new_entity)
 script.on_event(defines.events.on_robot_built_entity, on_new_entity)
 script.on_event(defines.events.script_raised_built, on_new_entity)
 script.on_event(defines.events.script_raised_revive, on_new_entity)
 script.on_init(OnInit)
+script.on_nth_tick(PeriodicUpdateInterval, OnNthTick)
